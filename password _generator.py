de84f3ccc0571e97d112c0ce42fcb66298075639
saar2666 <saar2666@gmail.comgit config --global user.name saar2666>
2021-12-08 20:55:39 +0200

    2

--- password _generator.py
+++ password _generator.py
@@ -1,6 +1,7 @@
 import random
 
 s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-passlen = input("choose pw length: ")
-p = "".join(random.sample(s, passlen ))
+#passlen = input("choose pw length: ")
+p = "".join(random.sample(s,8))
+print("8 char pw: ")
 print p
